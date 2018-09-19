988fdd34102144db79e5e95cb596dc12ee9c18a8
grzesiekb <grzegorz.bylica@gmail.com>
2018-09-19 14:33:58 +0200

    #704  Adding libraries to the vuejs responsible for the UI (main.js, package.json) and removing unused items

--- ScadaLTS-UI-1/src/router/index.js
+++ ScadaLTS-UI-1/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Test from '@/components/Test'
 import ExportImportPointHierarchy from '../components/ExportImportPointHierarchy'
 import SimpleComponentSVG from '../components/SimpleComponentSVG'
 
@@ -9,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/test',
       name: 'Test',
